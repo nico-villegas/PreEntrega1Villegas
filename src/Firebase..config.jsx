3219706a16efda1c389d2cb33e2c002ad0bc8221
nico-villegas <nicovillegas650@gmail.com>
2023-06-05 20:03:50 -0300

    probando algo

---
+++ src/Firebase..config.jsx
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAhTFq3o1683PLJvR234XqNUsSE5fEVPdQ",
+    authDomain: "prueba-1-153e2.firebaseapp.com",
+    projectId: "prueba-1-153e2",
+    storageBucket: "prueba-1-153e2.appspot.com",
+    messagingSenderId: "141302367355",
+    appId: "1:141302367355:web:83f68362139d26d2410ede"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
