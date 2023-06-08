bebcd0ef31a4d7f77c6c5acb15d78791ea960ecb
nico-villegas <nicovillegas650@gmail.com>
2023-06-08 16:08:33 -0300

    Creacion del checkout

--- src/routes/Rutas.jsx
+++ src/routes/Rutas.jsx
@@ -4,6 +4,7 @@ import Itemlistcontainer from '../components/ItemListContainer/ItemListContainer
 import ItemDetailContainer from '../components/ItemDetailContainer/ItemDetailContainer'
 import { CartProvider } from '../context/CartContext'
 import Cart from '../components/Cart/Cart'
+import Checkout from '../components/Chekout/Checkout'
 
 const Rutas = () => {
     return (
@@ -15,6 +16,7 @@ const Rutas = () => {
                     <Route path='/category/:categoryId' element={<Itemlistcontainer/>} />
                     <Route path='/item/:itemId' element={<ItemDetailContainer/>} />
                     <Route path='/cart' element={<Cart/>} />
+                    <Route path='/checkout' element={<Checkout/>} />
                     <Route path='*' element={<h1>404 NOT FOUND</h1>} />
                 </Routes>
             </CartProvider>
